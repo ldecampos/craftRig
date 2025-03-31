f3cb192ec422066ca80cc8d35e3395c9f801a9f2
luisdecampos <luisdecampos@gmail.com>
2025-03-31 20:39:11 +0200

    add naming utilities,  new validator (is_reference),  new getter (get_history),  new deletter (delete_history),

--- lib/nodes.py
+++ lib/nodes.py
@@ -1,9 +1,10 @@
 """
-This module provides a `Node` class that represents a node in a Maya scene.
+This module provides a 'Node' class that represents a node in a Maya scene.
 It includes methods for creating, querying, modifying, and deleting nodes
 """
-# Maya imports
+
 from maya import cmds
+from craftRig.lib import naming
 
 
 class Node(object):
@@ -18,6 +19,7 @@ class Node(object):
             name (str): The name of the node
             node_type (str): The type of the node
         """
+
         self.name = name
         self.node_type = node_type
 
@@ -51,7 +53,8 @@ class Node(object):
         >>> node.name
         'pCube1'
         """
-        selection = cmds.ls(selection=True)
+
+        selection = cmds.ls(selection=True) or []
 
         if not selection:
             return None
@@ -110,12 +113,34 @@ class Node(object):
 
         return cmds.lockNode(self.name, query=True, lock=True)[0]
 
+    def is_reference(self) -> bool:
+        """Check if the node is part of a reference.
+
+        Returns:
+            bool: True if the node is referenced, False otherwise.
+
+        Example:
+            >>> node = Node("pCube1")
+            >>> node.is_referenced()
+            False
+        """
+
+        if not self.node_exists():
+            return False
+
+        return cmds.referenceQuery(self.name, isNodeReferenced=True)
+
     # GETTERS
     def get_full_path(self) -> str:
         """Get the full path of the node.
 
         Returns:
             str: The full path of the node.
+
+        Example:
+        >>> node = Node('pCube1')
+        >>> node.get_full_path()
+        '|pCube1'
         """
 
         if not self.node_exists():
@@ -140,6 +165,22 @@ class Node(object):
 
         return None
 
+    def get_history(self, **kwargs) -> list[str]:
+        """Get the history of the node.
+
+        Returns:
+            List[str]: A list of history node names.
+
+        Example:
+            >>> node = Node("pCube1")
+            >>> node.get_history()
+        """
+
+        if not self.node_exists():
+            return []
+
+        return [x for x in cmds.listHistory(self.name, **kwargs) if not self.name in x] or []
+
     # SETTERS
     def lock_node(self, lock: bool = True) -> bool:
         """Lock or unlock the node.
@@ -155,6 +196,7 @@ class Node(object):
             >>> node.lock_node(False)
             True
         """
+
         if not self.node_exists():
             return False
 
@@ -166,13 +208,16 @@ class Node(object):
 
         Args:
             value (bool, optional): True to set intermediate, False to unset. Defaults to True.
+
         Returns:
             bool: True if the intermediate state was set or unset successfully, False otherwise.
+
         Example:
             >>> node = Node("pCube1")
             >>> node.set_intermediate(False)
             True
         """
+
         if not self.node_exists():
             return False
 
@@ -189,7 +234,13 @@ class Node(object):
 
         Returns:
             str: The name of the new node.
+
+        Example:
+            >>> node = Node("pCube1")
+            >>> node.duplicate_node()
+            'pCube2'
         """
+
         if not self.node_exists():
             return False
 
@@ -228,6 +279,23 @@ class Node(object):
 
         return False
 
+    def delete_history(self) -> bool:
+        """Delete the history of the node.
+
+        Returns:
+            bool: True if the history was deleted, False otherwise.
+
+        Example:
+            >>> node = Node("pCube1")
+            >>> node.delete_history()
+        """
+
+        if not self.node_exists():
+            return False
+        cmds.delete(self.get_history())
+
+        return True
+
     # METHODS
     def create_node(self, node_type: str, **kwargs) -> str:
         """Create node in the scene
@@ -249,6 +317,7 @@ class Node(object):
 
         return None
 
+    # NAMING UTILITIES
     def rename_node(self, new_name: str) -> bool:
         """Renames the node to a new name.
 
@@ -270,3 +339,135 @@ class Node(object):
         cmds.rename(self.name, new_name)
         self.name = new_name
         return new_name
+
+    def add_prefix(self, prefix: str, separator: str = '_') -> str:
+        """Add a prefix to the node name using a separator
+
+        Args:
+            prefix (str): The prefix to add.
+            separator (str, optional): The separator to use. Defaults to '_'.
+
+        Returns:
+            str: The new name of the node.
+
+        Example:
+            >>> node = Node("pCube1")
+            >>> node.add_prefix("myPrefix")
+            'myPrefix_pCube1'
+        """
+
+        new_name = naming.add_prefix(
+            text=self.name, prefix=prefix, separator=separator)
+
+        return self.rename_node(new_name)
+
+    def add_suffix(self, suffix: str, separator: str = '_') -> str:
+        """Add a suffix to the node name using a separator
+
+        Args:
+            suffix (str): The suffix to add.
+            separator (str, optional): The separator to use. Defaults to '_'.
+
+        Returns:
+            str: The new name of the node.
+
+        Example:
+            >>> node = Node("pCube1")
+            >>> node.add_suffix("mySuffix")
+            'pCube1_mySuffix'
+        """
+
+        new_name = naming.add_suffix(self.name, suffix, separator)
+
+        return self.rename_node(new_name)
+
+    def add_text(self, text: str) -> str:
+        """Add text to the node name in PascalCase
+
+        Args:
+            text (str): The text to add.
+
+        Returns:
+            str: The new name of the node.
+
+        Example:
+            >>> node = Node("pCube1")
+            >>> node.add_text("test")
+            'pCube1Test'
+        """
+
+        new_name = naming.add_text(text=self.name, text_to_add=text)
+
+        return self.rename_node(new_name)
+
+    def increment_digit(self, digits: int = None) -> str:
+        """Increment the last number in the node.
+
+        Args:
+            digits(int, optional): Number of digits for the number to increment.
+                                    Defaults to None
+
+        Returns:
+            str: The new name of the node.
+
+        Example:
+            >>> node = Node("pCube1")
+            >>> node.rename_with_increment_digit(3)
+            'pCube002'
+        """
+
+        new_name = naming.increment_digit(self.name, digits=digits)
+
+        return self.rename_node(new_name)
+
+    def decrement_digit(self) -> str:
+        """Decrement the last number in the node.
+
+        Returns:
+            str: The new name of the node.
+
+        Note:
+            If the text does not contain numbers, it will return the original text
+            If the number is 0 or 1, it will remove it
+
+        Example:
+            >>> node = Node("pCube1")
+            >>> node.rename_with_decrement_digit()
+            'pCube'
+        """
+
+        new_name = naming.decrement_digit(text=self.name)
+
+        return self.rename_node(new_name)
+
+    def increment_character(self):
+        """
+        Rename the node with an incremented character.
+
+        Returns:
+            str: The new name of the node.
+
+        Example:
+            >>> node = Node("test")
+            >>> node.rename_with_increment_character()
+            'tess'
+        """
+
+        new_name = naming.increment_character(text=self.name)
+
+        return self.rename_node(new_name)
+
+    def decrement_character(self):
+        """Decrement a letter sequence in a manner similar to Excel column naming
+
+        Returns:
+            str: The new name of the node.
+
+        Example:
+            >>> node = Node("pCube1")
+            >>> node.rename_with_decrement_character()
+        """
+
+        new_name = naming.decrement_character(text=self.name)
+
+        return self.rename_node(new_name)
